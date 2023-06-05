1046f52ff842c936f3ec89c55a02b6950fa351f9
rp-bot_ubuntu <legacy.business18@gmail.com>
2023-06-05 00:56:00 -0400

    new packages

--- src/filter_midi_tracks.py
+++ src/filter_midi_tracks.py
@@ -1,10 +1,11 @@
 from __init__ import *
 from data_cleaning import Open, Stream, MakeMIDI, ChordEval
-from music21 import chord, note
+from music21 import chord, note, stream
 import os
 import numpy as np
 import pandas as pd
 from pprint import pprint
+from tqdm import tqdm
 
 
 def get_file_paths():
@@ -25,12 +26,35 @@ def filter_chords(midi_data):
             if isinstance(element, chord.Chord):
                 chord_array = []
                 for i, n in enumerate(element):
-                    chord_array.append(
-                        np.array([n.pitch, n.volume.velocity, element.offset]))  # type: ignore
+                    chord_array.append(n)
                 chords_array.append(chord_array)
         if chords_array:
             parts_array.append(chords_array)
     return parts_array
+
+
+def write_midi_files(midi_data):
+    streams = []
+    for track in tqdm(midi_data):
+
+        for chord in track:
+            s = stream.Stream()
+            for pitch, velocity, offset in chord:
+                # Create a note with the given pitch
+                n = note.Note(midi=pitch.ps)
+                n.volume.velocity = velocity  # Set the velocity
+                n.offset = offset  # Set the offset
+                s.append(n)  # Add the note to the stream
+            streams.append(s)
+
+    # Create a Score and add each stream as a part
+    score = stream.Score()
+    for s in streams:
+        part = stream.Part(s)
+        score.insert(0, part)
+
+    # Write the Score to a MIDI file
+    score.write('midi', 'output_file.mid')
 
 
 if __name__ == "__main__":
@@ -38,4 +62,8 @@ if __name__ == "__main__":
     midi_objects = Open.open_midi_files(midi_files)
 
     filtered_midi_dict = filter_chords(midi_objects[0])
-    pprint(filtered_midi_dict)
+
+    # write_midi_files(filtered_midi_dict)
+
+    x = filtered_midi_dict[1]  # [part][chord][note]
+    pprint(filtered_midi_dict, indent=2)
